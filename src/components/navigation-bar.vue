8d0b76da66d8fadc616a5a5895ed13d69b416b22
hareeshcs33 <hareeshcs33@gmail.com>
2021-04-18 16:14:48 +0530

    ui update

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
+    <div class="nav-block">
         <router-link to="/home">Home</router-link>
     </div>
 </template>
+<style scoped>
+.nav-block {
+    background: #ccc;
+}
+.nav-block a {
+    display: inline-block;
+    text-decoration: none;
+    padding: 5px 10px;
+}
+</style>
