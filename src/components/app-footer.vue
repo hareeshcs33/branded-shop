2f3e28c37feb0a74cd8a6b9f70ca927d908c60dd
hareeshcs33 <hareeshcs33@gmail.com>
2021-07-19 22:19:40 +0530

    modal box added

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,8 +1,33 @@
 <template>
     <div class="footer-wrapper">
         <div>footer</div>
+        <div>Terms and conditions</div>
+        <button class="btn btn-link" @click="openModal">Open Modal</button>
+        <div>
+            <modal v-model="modalOpen"></modal>
+        </div>
     </div>
 </template>
+<script>
+import Modal from './modal.vue';
+
+export default {
+    data(){
+        return {
+            modalOpen: false,
+        }
+    },
+    methods: {
+        openModal(){
+            console.log("openModal", this.modalOpen);
+            this.modalOpen = !this.modalOpen;
+        }
+    },
+    components: {
+        Modal,
+    }
+}
+</script>
 <style scoped>
 .footer-wrapper {
     color: #fff;
---
+++ src/components/modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal m-auto" v-show="value">
+    <div class="modal-content p-5">
+      <h1>Modal Box........</h1>
+      <div>{{ value }}</div>
+      <button @click.prevent="close" class="btn btn-danger">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "modal",
+  props: {
+    value: {
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    close() {
+      this.$emit("input", !this.value);
+    }
+  }
+};
+</script>
+<style scoped>
+.modal {
+  background-color: rgba(0, 0, 0, 0.8);
+  display: block;
+}
+.modal-content {
+max-width: 70%;
+  color: #000;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
